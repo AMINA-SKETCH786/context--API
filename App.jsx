d389d9ec85732236c531a280e282a772b0a8b7ec
AMINA-SKETCH786 <aminna.readings@gmail.com>
2025-01-18 21:45:41 -0800

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Login from './components/Login'
+import Profile from './components/Profile'
+import UserContextProvider from './components/context/UserContextProvider'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <UserContextProvider>
+      <div>React Context API</div>
+      <Login/>
+      <Profile/>
+    </UserContextProvider>
+  )
+}
+
+export default App
